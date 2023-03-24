a5e5822ccb7193eed8c1c5f0a57d11b2fb6c5f75
Lisbeth <lisbethperezcm@gmail.com>
2023-03-23 23:09:40 -0400

    Cambios en el formulario de contacto y visualizacion del loanding

--- assets/sendmessage.php
+++
@@ -1,8 +0,0 @@
-$name =$_POST ['name'];
-$subject = $_POST['subject'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-
-$destinatario = "lisbethperezcm@gmail.com"
-
-mail($destinatario,$subject,$message);
