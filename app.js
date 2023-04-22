9fd12dc90dad8570c9bf7147c957405833dc3432
Lisbeth <lisbethperezcm@gmail.com>
2023-04-22 07:53:22 -0400

    Modificaciones para mejoras en la parte de contacto y el menu responsive, tambien se mejoran animaciones para el scroll del sitio web

--- app.js
+++ app.js
@@ -1,49 +1,92 @@
-const menu = document.querySelector('.menu');
-const toggle = document.querySelector('.toggle');
+const toggleButton = document.querySelector('.toggle');
+const menu = document.querySelector('#menu');
+const navbar = document.querySelector('.navbar');
+const brand = document.querySelector('.brand');
+
 const icon = document.querySelector('.fa-bars');
 var contenedor = document.querySelector('#contenedor-load');
 var hero = document.getElementById("hero-img");
 
 const header = document.getElementById("header");
 
+const sections = document.querySelectorAll('section');
+
+window.addEventListener('scroll', () => {
+  let current = '';
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute('id');
+    }
+  });
+  if (window.innerWidth > 800) {
+  activateNavbar(current);}
+});
+
+function activateNavbar(current) {
+  const links = document.querySelectorAll('.link-item');
+  
+  links.forEach((link) => {
+
+    link.classList.remove('active');
+
+    if (link.href === current) {
+      link.classList.add('active');
+    }
+  });
+
+}
 
  // EVENT LOANDING
+
 if (hero.style.visibility == "false"){
 
 }else{
     contenedor.style.display="none";
 }
 
+toggleButton.addEventListener('click', toggleMenu);
+menu.addEventListener('click', toggleMenu);
  // EVENT CLOSE MENU
+ /*
 toggle.addEventListener("click",closeMenu);
 menu.addEventListener("click", closeMenu);
 
 
-
+*/
 
 
  // EVENT CHANGE NAVBAR BACKGROUND
-document.addEventListener("scroll",backgroundNavbar);
+document.addEventListener("scroll",() => {
+  if (window.innerWidth > 800) {
+backgroundNavbar();
+  }
+
+});
 /*inputs.addEventListener("click", removePlace);*/
 
 // EVENT ADD COLOR ACTIVE NAVBAR
 /*
 menu.addEventListener("click", activeNavbar);*/
-links.addEventListener("click",activeNavbar);
-
-
+//links.addEventListener("click",activeNavbar);
+/*
+links.addEventListener('click', function() {
+    section1.scrollIntoView({ behavior: 'smooth' });
+  });
+*/
 
  // FUNCTION NAVBAR BACKGROUND
 function backgroundNavbar(){
     if( window.scrollY < 100){
-        header.style.backgroundColor="transparent";
+        navbar.style.backgroundColor="transparent";
     }else if( window.scrollY > 100){
-        header.style.backgroundColor="#000000";
+        navbar.style.backgroundColor="#000000";
     }  
 }
 
  // FUNCTION NAVBAR ACTIVE
- function activeNavbar(link){
+ /*function activeNavbar(link){
     var links = document.querySelectorAll('.link-item');
    
     links.forEach(function(item) {
@@ -52,12 +95,24 @@ function backgroundNavbar(){
         }else{
             item.classList.remove("active");
         }
-    })
+    });
     
- 
+    links.forEach(link => {
+        link.addEventListener('click', e => {
+          // Previene el comportamiento por defecto del enlace
+          e.preventDefault();
+      
+          // Selecciona el elemento al que se hará scroll
+          const target = document.querySelector(link.getAttribute('href'));
+      
+          // Hace scroll suave al elemento seleccionado
+          target.scrollIntoView({
+            behavior: 'smooth'
+          });
+        })});
     }
 
-
+ 
     
  /*
     var links =document.querySelectorAll('.link-item')
@@ -71,7 +126,22 @@ function backgroundNavbar(){
         }; 
   */
 
+
+        function toggleMenu() {
+          if (menu.classList.contains('active')) {
+            menu.classList.remove('active');
+          
+          } else {
+            menu.classList.add('active');
+            navbar.style.position="fixed";
+            
+          /*  brand.style.transform = 'translateX(-50%)';*/
+          }
+        }
+        
+
  // FUNCTION CLOSE MENU
+/*
 function closeMenu(){
    
         if (window.innerWidth <= 800) {
@@ -90,5 +160,5 @@ function closeMenu(){
     }
 
 }
-
+*/
 
